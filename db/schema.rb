45cbf15630ae7c97fe67e3f6ffdf35e2e339b7ba
googli1 <googlthegoogl@gmail.com>
2017-05-10 13:04:56 +0500

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509170722) do
+ActiveRecord::Schema.define(version: 20170509171358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170509170722) do
     t.string   "name"
     t.string   "address"
     t.string   "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "room_types", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
